8ceacd8b824bbdabdfdb4ae624da39e8f0abb394
Elinabai <elinabazarbaikyzy017@gmail.com>
2023-02-07 20:20:33 +0600

    sfgvbn

---
+++ app/src/main/java/com/geektech/android2dz21/utils/PreferenceHelper.kt
@@ -0,0 +1,17 @@
+package com.geektech.android2dz21.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class PreferenceHelper {
+
+    private lateinit var sharedPreference: SharedPreferences
+
+    fun unit(context: Context){
+        sharedPreference = context.getSharedPreferences("settings",Context.MODE_PRIVATE)
+    }
+
+    var saveBoolean: Boolean
+    set(value) = sharedPreference.edit().putBoolean("key", value).apply()
+    get() = sharedPreference.getBoolean("key",false)
+}
